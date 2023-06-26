e88e7c2db0ad253528588246e8957690a37403c3
TonLeon <55150238+TonLeon@users.noreply.github.com>
2023-06-26 15:03:06 -0400

    images display and search function

--- 003_source.md
+++ 003_source.md
@@ -8,3 +8,23 @@ number: 003
 
 MY WONDEFUL SOURCES
 <!-- <iframe width="420" height="315" src="https://www.youtube.com/watch?v=EmSrQCDsMv4&t=1282s&ab_channel=BillRaymond" frameborder="0" ></iframe> -->
+
+
+
+{% assign pages = site.pages | where_exp: "item", "item.title" | where_exp: "item", "item.title != 'Search'" %}
+
+{% assign collections = site.mindoc_images %}
+
+{% assign pagesAndPosts = pages | concat: collections %}
+
+{% for pageAndPost in pagesAndPosts %}
+    {% for text in pagesAndPosts %}
+    {{ text.url | jsonify }}
+    {% endfor %}
+{% endfor %}
+
+
+
+
+
+
--- assets/js/search.js
+++ assets/js/search.js
@@ -13,27 +13,41 @@ var index = elasticlunr(function () {
 });
 
 // Add to this index the proper metadata from the Jekyll content
-{% assign count = 0 %}{% for text in site.pages %}
+{% assign pages = site.pages | where_exp: "item", "item.title" | where_exp: "item", "item.title != 'Search'" %}
+
+{% assign collections = site.mindoc_images %}
+
+{% assign pagesAndCollections = pages | concat: collections %}
+
+
+{% assign count = 0 %}{% for text in pagesAndCollections %}
 {% if text.title != "Search" %}
+{% if text.title != null %}
 index.addDoc({
   title: {{text.title | jsonify}},
   author: {{text.author | jsonify}},
   layout: {{text.layout | jsonify}},
   content: {{text.content | jsonify | strip_html}},
   id: {{count}}
-});{% assign count = count | plus: 1 %}{% endif %}{% endfor %}
+});{% assign count = count | plus: 1 %}{% endif %}{% endif %}{% endfor %}
 console.log( jQuery.type(index) );
 
 // Builds reference data (maybe not necessary for us, to check)
-var store = [{% for text in site.pages %}
+var store = [
+  {% assign pages = site.pages | where_exp: "item", "item.title" | where_exp: "item", "item.title != 'Search'" %}
+  {% assign collections = site.mindoc_images %}
+  {% assign pagesAndCollections = pages | concat: collections %}
+  
+  {% for text in pagesAndCollections %}
   {% if text.title != "Search" %}
+  {% if text.title != null %}
   {
   "title": {{text.title | jsonify}},
   "author": {{text.author | jsonify}},
   "layout": {{ text.layout | jsonify }},
   "link": {{text.url | jsonify}},
 }
-{% unless forloop.last %},{% endunless %}{% endif %}{% endfor %}]
+{% unless forloop.last %},{% endunless %}{% endif %}{% endif %}{% endfor %}]
 
 // Query
 var qd = {}; // Gets values from the URL
@@ -49,14 +63,16 @@ function doSearch() {
   var query = $('input#search').val();
 
   // The search is then launched on the index built with Lunr
+
+  
   var result = index.search(query);
   resultdiv.empty();
   if (result.length == 0) {
-    resultdiv.append('<p class="">No results found.</p>');
+    resultdiv.append('<p class="search_result">No results found.</p>');
   } else if (result.length == 1) {
-    resultdiv.append('<p class="">Found '+result.length+' result</p>');
+    resultdiv.append('<p class="search_result">Found '+result.length+' result:</p>');
   } else {
-    resultdiv.append('<p class="">Found '+result.length+' results</p>');
+    resultdiv.append('<p class="search_result">Found '+result.length+' results:</p>');
   }
   // Loop through, match, and add results
   for (var item in result) {
